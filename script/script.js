43a35fe732a38c8b65f22933b2f2895f831382dc
r2d23e <kildiyarov.timur13@gmail.com>
2023-03-11 12:09:32 +0500

    cw

---
+++ script/script.js
@@ -0,0 +1,24 @@
+let n = 0
+// while (n < 100) {
+//     n = n + 3
+//     document.write(n + "<br>")
+// }
+
+
+
+// do {
+//     n = n + 5
+//     document.write(n + '<br>')
+// } while( n <= 100)
+
+
+let zv1 = "*******"
+
+
+
+do {
+    zv1 = zv1.replace(/ */,"")
+
+} while (zv1.length < 1);
+
+
