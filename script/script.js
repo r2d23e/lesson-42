132724868380fd13f60f94739d3e76a5c454fce0
r2d23e <kildiyarov.timur13@gmail.com>
2023-03-14 09:18:52 +0500

    hw

--- script/script.js
+++ script/script.js
@@ -12,13 +12,40 @@ let n = 0
 // } while( n <= 100)
 
 
-let zv1 = "*******"
+// let zv1 = "*******"
 
 
 
+// do {
+//     zv1 = zv1.replace(/ */,"")
+
+// } while (zv1.length < 1);
+
+
+//дз начало
+let star1 = ""
+let star2 = "*"
+
+while (star1.length <= 7) {
+    star1 += star2
+    document.write(star1 + "<br>")
+    console.log(star1)
+}
+
+
+let stars = "********"
+console.log(stars);
 do {
-    zv1 = zv1.replace(/ */,"")
+    stars = stars.replace("*", "")
+    document.write(stars + "<br>")
+    console.log(stars);
+} while (stars.length >= 1)
 
-} while (zv1.length < 1);
 
 
+
+while (star1.length <= 10) {
+    star1 += star2
+     document.write(star1 + "<br>")
+    console.log(star1);
+}
